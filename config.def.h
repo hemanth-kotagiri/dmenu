898e5c455f36ad899179f040862f3dd4523ed8c7
Hemanth <hemanth.kotagiri43@gmail.com>
2021-10-17 19:29:53 +0530

    Update README

--- config.def.h
+++ config.def.h
@@ -7,12 +7,13 @@ static const char *fonts[] = {
 	"monospace:size=10"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
-static const char *colors[SchemeLast][2] = {
-	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
-	[SchemeOut] = { "#000000", "#00ffff" },
-};
+//static const char *colors[SchemeLast][2] = {
+	//[>     fg         bg       <]
+	//[SchemeNorm] = { "#bbbbbb", "#222222" },
+	//[SchemeSel] = { "#eeeeee", "#005577" },
+	//[SchemeOut] = { "#000000", "#00ffff" },
+//};
+#include "/home/hemanth/.cache/wal/colors-wal-dmenu.h"
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
 
